2af961fec14535b88bfedaa876ad2ba92fafe5f5
nikitha-git05 <nikithalokesh5@gmail.com>
2025-03-31 20:51:25 +0530

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "terraform_instance" {
   ami = "ami-0d0f28110d16ee7d6"
   instance_type = "t2.micro"
   availability_zone = "us-east-2a"
-  key_name = "tester"
+  key_name = "ec2-jenkins"
   tags = {
     Name = "Terraform_Instance"
   }
